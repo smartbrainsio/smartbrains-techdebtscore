1b44563c8d0448c87d6944c035bda2cd96ff2a5a
Jimmy Hurff <jhurff@brightmove.com>
2020-12-27 14:40:03 -0600

    Basic react form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API } from 'aws-amplify';
+import logo from './logo.svg';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { listOrganizationScores } from './graphql/queries';
+import { createOrganizationScore as createOrganizationScoreMutation, deleteOrganizationScore as deleteOrganizationScoreMutation } from './graphql/mutations';
+
+const initialFormState = { organizationName: '', orgScore: '' }
 
 function App() {
+
+  const [organizationScores, setOrganizationScores] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchOrganizationScores();
+  }, []);
+
+  async function fetchOrganizationScores() {
+    const apiData = await API.graphql({ query: listOrganizationScores });
+    setOrganizationScores(apiData.data.listOrganizationScores.items);
+  }
+
+  async function createOrganizationScore() {
+    if (!formData.organizationName || !formData.orgScore) return;
+    await API.graphql({ query: createOrganizationScoreMutation, variables: { input: formData } });
+    setOrganizationScores([ ...organizationScores, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteOrganizationScore({ id }) {
+    const newOrganizationScoresArray = organizationScores.filter(organizationScore => organizationScores.id !== id);
+    setOrganizationScores(newOrganizationScoresArray);
+    await API.graphql({ query: deleteOrganizationScoreMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to SmartBrains.io!</h1>
+        <h1>Welcome to SmartBrains.io Tech Debt Scores App!</h1>
       </header>
+      <input
+        onChange={e => setFormData({ ...formData, 'organizationName': e.target.value})}
+        placeholder="Organization name"
+        value={formData.organizationName}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'orgScore': e.target.value})}
+        placeholder="Score"
+        value={formData.orgScore}
+      />
+      <button onClick={createOrganizationScore}>Create Score</button>
+      <div style={{marginBottom: 30}}>
+        {
+          organizationScores.map(organizationScore => (
+            <div key={organizationScore.id || organizationScore.organizationName}>
+              <h2>{organizationScore.organizationName}</h2>
+              <p>{organizationScore.orgScore}</p>
+              <button onClick={() => deleteOrganizationScore(organizationScore)}>Delete Score</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
---
+++ src/graphql/mutations.js
@@ -0,0 +1,180 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createScoreFeature = /* GraphQL */ `
+  mutation CreateScoreFeature(
+    $input: CreateScoreFeatureInput!
+    $condition: ModelScoreFeatureConditionInput
+  ) {
+    createScoreFeature(input: $input, condition: $condition) {
+      id
+      organizationScoreid
+      featureName
+      featureValue
+      featureWeight
+      organizationscoreID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateScoreFeature = /* GraphQL */ `
+  mutation UpdateScoreFeature(
+    $input: UpdateScoreFeatureInput!
+    $condition: ModelScoreFeatureConditionInput
+  ) {
+    updateScoreFeature(input: $input, condition: $condition) {
+      id
+      organizationScoreid
+      featureName
+      featureValue
+      featureWeight
+      organizationscoreID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteScoreFeature = /* GraphQL */ `
+  mutation DeleteScoreFeature(
+    $input: DeleteScoreFeatureInput!
+    $condition: ModelScoreFeatureConditionInput
+  ) {
+    deleteScoreFeature(input: $input, condition: $condition) {
+      id
+      organizationScoreid
+      featureName
+      featureValue
+      featureWeight
+      organizationscoreID
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const createOrganizationScore = /* GraphQL */ `
+  mutation CreateOrganizationScore(
+    $input: CreateOrganizationScoreInput!
+    $condition: ModelOrganizationScoreConditionInput
+  ) {
+    createOrganizationScore(input: $input, condition: $condition) {
+      id
+      organizationName
+      createDate
+      orgScore
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      ScoreFeatures {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const updateOrganizationScore = /* GraphQL */ `
+  mutation UpdateOrganizationScore(
+    $input: UpdateOrganizationScoreInput!
+    $condition: ModelOrganizationScoreConditionInput
+  ) {
+    updateOrganizationScore(input: $input, condition: $condition) {
+      id
+      organizationName
+      createDate
+      orgScore
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      ScoreFeatures {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const deleteOrganizationScore = /* GraphQL */ `
+  mutation DeleteOrganizationScore(
+    $input: DeleteOrganizationScoreInput!
+    $condition: ModelOrganizationScoreConditionInput
+  ) {
+    deleteOrganizationScore(input: $input, condition: $condition) {
+      id
+      organizationName
+      createDate
+      orgScore
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+      ScoreFeatures {
+        nextToken
+        startedAt
+      }
+    }
+  }
+`;
+export const createFeature = /* GraphQL */ `
+  mutation CreateFeature(
+    $input: CreateFeatureInput!
+    $condition: ModelFeatureConditionInput
+  ) {
+    createFeature(input: $input, condition: $condition) {
+      id
+      name
+      description
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateFeature = /* GraphQL */ `
+  mutation UpdateFeature(
+    $input: UpdateFeatureInput!
+    $condition: ModelFeatureConditionInput
+  ) {
+    updateFeature(input: $input, condition: $condition) {
+      id
+      name
+      description
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteFeature = /* GraphQL */ `
+  mutation DeleteFeature(
+    $input: DeleteFeatureInput!
+    $condition: ModelFeatureConditionInput
+  ) {
+    deleteFeature(input: $input, condition: $condition) {
+      id
+      name
+      description
+      _version
+      _deleted
+      _lastChangedAt
+      createdAt
+      updatedAt
+    }
+  }
+`;
